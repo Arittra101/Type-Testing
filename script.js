a116c56bc157377aec242514b9f079fdb67c1b9b
Avishek Das <avishek51775@gmail.com>
2023-04-15 01:48:15 +0600

    updated

--- script.js
+++ script.js
@@ -5,17 +5,26 @@ let time = 0;
 let mistake = 0;
 let char_to_arry;
 let quotes;
-let timer = 10;
+let timer = 30;
 let start = 0;
+let numberofchar = 0;
+let front_time = 0;
+let char_type = 1;
+let alpha = 0;
 // random_query.innerHTML = `;
 const level_one = () => {
 
-    quotes = '"Hello I can do anything";';
+    //  quotes = 'cout<<"Hello I can do anything";';
+    quotes = 'cout<<"The quick brown fox jumps over the lazy dog."';
+    numberofchar = quotes.length;
     convertQuotes_To_array(quotes)
 };
 const level_two = () => {
+    console.log("level two");
+    // random_query.innerHTML="";
+    quotes = 'cout<<"The quick brown fox jumps over the lazy dog"';
+    convertQuotes_To_array(quotes);
 
-    random_query.innerHTML = 'cout<<"The quick brown fox jumps over the lazy dog"';
 };
 const level_three = () => {
 
@@ -56,8 +65,11 @@ user_input.addEventListener("input", () => {
 
     //work with input
     let array_user_input = user_input.value.split(""); //convert all input char into array element
-
-
+    char_type = array_user_input.length;
+    if (array_user_input[0] >= 'A' && array_user_input[0] <= 'Z' || array_user_input[0] >= 'a' && array_user_input[0] <= 'z') {
+        alpha = 1;
+        console.log("Ds");
+    }
     //---
 
     //now check with quotes char and input char
@@ -86,6 +98,13 @@ user_input.addEventListener("input", () => {
             }
             document.querySelector("#mistakes").innerHTML = mistake;
         }
+        let if_all = quoteChars.every(check_all_success);
+        function check_all_success(value) {
+            return value.classList.contains("success");
+        }
+        if (if_all) {
+            displayResult();
+        }
     });
 
 
@@ -94,17 +113,20 @@ user_input.addEventListener("input", () => {
 
 
 setInterval(() => {
-    if(start)
-    {
-        if (timer > 0) timer--;
+    if (start) {
+        if (timer > 0) 
+        {
+            timer--;
+            front_time++;
+        }
         if (timer == 0) {
-    
+
             displayResult();
         }
         let timer_html = document.querySelector('#timer');
         timer_html.innerHTML = timer + "Sec";
     }
-  
+
 
 
 }, 1000);
@@ -116,35 +138,70 @@ function startTest() {
     start_btn = document.querySelector('#stop-test').style.display = "block";
     user_input.disabled = false;
     start = 1;
-    // let result = document.querySelector('.result');
-    // result.style.display = "none";
+
+
+
 
 
 }
 
 
 function displayResult() {
+    calculation();
+
+
+}
+
+function calculation() {
+    let timer_html = document.querySelector('#accuracy');
+    let speed_html = document.querySelector('#wpm');
+    let mistake_html = document.querySelector('#mis');
+    let rs = document.querySelector('#re');
+    if (char_type > 1 || alpha == 1) {
+
+
+        let numberOfCorrect = char_type - mistake;
+        console.log(numberOfCorrect);
+
+        let cal = Math.round((numberOfCorrect / char_type) * 100.00);
+        timer_html.innerHTML = cal + "%";
+
+        mistake_html.innerHTML = mistake + " times";
+        console.log("typed " + char_type);
+
+        let spd =  Math.round((char_type/5)/(front_time/60));
+        speed_html.innerHTML = spd + " WPM";
+        //window_load();
+    }
+    else {
+        rs.innerHTML = "Please! Type SomeThing";
+        timer_html.innerHTML = "0";
+    }
+
+    console.log(char_type);
     iniital();
 
-    let result = document.querySelector('.result');
-    result.style.display = "block";
-    
 }
 window.onload = () => {
+
+    window_load();
+}
+function window_load() {
+    //if(alpha==1) level_two();
     level_one();
     let start_btn = document.querySelector('#stop-test').style.display = "none";
     user_input.disabled = true;
     let timer_html = document.querySelector('#timer');
 
-    timer_html.innerHTML = 60 + "Sec";
-
+    timer_html.innerHTML = 30 + " Sec";
+    let result = document.querySelector('.result');
+    result.style.display = "block";
 }
-function iniital()
-{
-    start=0;
-    mistake=0;
+function iniital() {
+    start = 0;
+    mistake = 0;
     level = 1;
-    timer =10;
+    timer = 30;
 
     let timer_html = document.querySelector('#timer');
     timer_html.innerHTML = 60 + "Sec";
@@ -152,7 +209,12 @@ function iniital()
     let start_btn = document.querySelector('#start-test').style.display = "block";
     start_btn = document.querySelector('#stop-test').style.display = "none";
 
-    let level_inner  = document.querySelector("#level");
+    let level_inner = document.querySelector("#level");
     level_inner.innerHTML = 1;
+    user_input.disabled = true;
+
+
+
+
 }
 
